3ab96a50fffcca316d211c04283209dc67dd0caa
Gomin <bighead0831@naver.com>
2025-05-10 22:18:47 +0900

    Title: 최대로 겹치는 지점, Time: 187ms, Memory: 15MB, Status: Passed - Codetree

--- 250510/최대로 겹치는 지점/maximum-overlapped-points.js
+++ 250510/최대로 겹치는 지점/maximum-overlapped-points.js
@@ -7,7 +7,7 @@ for (let i = 1; i <= n; i++) {
   segments.push(input[i].split(' ').map(Number));
 }
 
-let arr = Array(100).fill(0);
+let arr = Array(101).fill(0);
 for(let i=0; i<n; i++) {
     for(let j=segments[i][0]; j<=segments[i][1]; j++) {
         arr[j] += 1;
