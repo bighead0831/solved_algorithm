5555f6b46beb5e9d4b6010605e41d01eefbce658
Gomin <bighead0831@naver.com>
2025-05-26 23:41:57 +0900

    Title: 연속되는 수 2, Time: 176ms, Memory: 15MB, Status: Passed - Codetree

--- 250526/연속되는 수 2/continuous-number2.js
+++ 250526/연속되는 수 2/continuous-number2.js
@@ -5,13 +5,21 @@ const n = Number(input[0]);
 const arr = input.slice(1, n + 1).map(Number);
 
 
-function checkGroup (arr) {
+function checkMaxGroup (arr) {
     let cnt = 1;
+    let max = 1;
+
     for(let i=1; i<arr.length; i++) {
-        if(arr[i] != arr[i-1]) cnt++;
+        if(arr[i] === arr[i-1]) {
+            cnt++;
+            if(max < cnt) max = cnt;
+        }
+        else {
+            cnt = 1;
+        }
     }
 
-    return cnt;
+    return max;
 }
 
-console.log(checkGroup(arr));
+console.log(checkMaxGroup(arr));
