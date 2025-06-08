02c49e06202ae279189db4acfc247f0864b8f6b4
Gomin <bighead0831@naver.com>
2025-06-08 11:50:43 +0900

    Title: 작은 구슬의 이동, Time: 183ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250608/작은 구슬의 이동/small-marble-movement.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, t] = input[0].split(" ").map(Number);
+let [r, c, d] = input[1].split(" ");
+r = Number(r);
+c = Number(c);
+
+// Please Write your code here.
+let dx = [1, 0, 0, -1]; // x by (R, U, D, L)
+let dy = [0, 1, -1, 0]; // y by (R, U, D, L)
+const mapper = {'R': 0, 'U': 1, 'D': 2, 'L': 3};
+let dirNum = mapper[d];
+
+// 1~n범위 내의 x, y 검사
+function inRange(x, y) {return x>=0 && x<n && y>=0 && y<n;}
+
+r-=1, c-=1;
+for(let i=0; i<t; i++) {
+    c=c+dx[dirNum];
+    r=r+dy[dirNum];
+    if(inRange(c,r) === false) {
+        c=c-dx[dirNum];
+        r=r-dy[dirNum];
+        dirNum = (dirNum+3)%3;
+    }
+}
+r+=1, c+=1;
+console.log(r, c);
