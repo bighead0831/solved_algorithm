9ec6dd29c59a2b6084685b09adfc878a48cf18c7
Gomin <bighead0831@naver.com>
2025-06-11 22:12:12 +0900

    Title: 수 채우기, Time: 207ms, Memory: 15MB, Status: Passed - Codetree

--- 250611/수 채우기/fill-in-number.js
+++ 250611/수 채우기/fill-in-number.js
@@ -10,10 +10,11 @@ let coin5 = 0;
 coin5 = parseInt(n/5);
 coin2 = parseInt((n-coin5*5)/2);
 
+
 while(n!=coin5*5+coin2*2) {
-    coin5 -= 1;
+    if(coin5 != 0) {coin5 -= 1;}
     coin2 = parseInt((n-coin5*5)/2);
-    if(coin5 === 0 && coin2*2 != n) {coin2 = -1;} // 2원, 5원 동전으로 n원을 만들 수 없을 때는 -1
+    if(coin5 === 0 && coin2*2 != n) {coin2 = -1; break;} // 2원, 5원 동전으로 n원을 만들 수 없을 때는 -1
 }
 
 console.log(coin5+coin2);
