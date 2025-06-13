faf3bebae75aed25a690156f6265180e6551614f
Gomin <bighead0831@naver.com>
2025-06-14 01:55:43 +0900

    Title: 정수 N개의 합 2, Time: 210ms, Memory: 18MB, Status: Passed - Codetree

--- 250614/정수 N개의 합 2/sum-of-n-integers-2.js
+++ 250614/정수 N개의 합 2/sum-of-n-integers-2.js
@@ -5,14 +5,17 @@ const [n, k] = input[0].trim().split(' ').map(Number);
 const arr = input[1].trim().split(' ').map(Number);
 
 // Please Write your code here.
-let result = new Array(n-k).fill(0);
+let prefixSum = new Array(n).fill(0);
 
-let big = 0;
+prefixSum[0] = arr[0];
+for(let i=1; i<n; i++) {
+    prefixSum[i] = prefixSum[i-1]+arr[i];
+}
+
+let tmp = 0, big = prefixSum[k-1];
 for(let i=0; i<n-k; i++) {
-    for(let j=0; j<k; j++) result[i] += arr[i+j];
-    if(big<result[i]) {
-        big = result[i];
-    }
+    tmp = prefixSum[i+k]-prefixSum[i];
+    if(big < tmp) big = tmp;
 }
 
 console.log(big);
