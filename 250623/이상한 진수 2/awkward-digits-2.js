bc5e75055ded84bcfdbcb8a54d116e0253088be3
Gomin <bighead0831@naver.com>
2025-06-23 20:41:20 +0900

    Title: 이상한 진수 2, Time: 189ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250623/이상한 진수 2/awkward-digits-2.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const a = input[0].split('').map(Number);
+
+// Please Write your code here.
+let getChange = false;
+let i=0;
+while(getChange = true) {
+    if(a[i] === 0) {
+        getChange = true;
+        a[i] = 1;
+        break;
+    } else {
+        i++;
+        if(i===a.length) break;
+    }
+}
+
+let sum = 0;
+for(let j=0; j<a.length; j++) {
+    sum += a[j]*(2**((a.length-1)-j));
+}
+
+console.log(sum);
