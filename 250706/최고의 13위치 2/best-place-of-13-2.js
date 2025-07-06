90ec3b85b44bc0ea143fe8deb032bf792d3d1160
Gomin <bighead0831@naver.com>
2025-07-06 12:17:40 +0900

    Title: 최고의 13위치 2, Time: 190ms, Memory: 15MB, Status: Passed - Codetree

--- 250706/최고의 13위치 2/best-place-of-13-2.js
+++ 250706/최고의 13위치 2/best-place-of-13-2.js
@@ -10,15 +10,15 @@ let maxCoin = 0;
 for(let i=0; i<n; i++) {
     for(let j=0; j<n-2; j++) {
         let sg1_coin = 0;
-        for(let c=j; c<j+3; c++) {
+        for(let c=j; c<=j+2; c++) {
             if(grid[i][c] === 1) sg1_coin++;
         }
         for(let r=i; r<n; r++) {
             for(let c=0; c<n; c++) {
                 let sg2_coin = 0;
-                if(r===i && c>=j && c<j+3) continue;
+                if(r===i && c<=j+2) continue;
                 if(inRange(r,c+2)) {
-                    for(let k=c; k<c+3; k++)
+                    for(let k=c; k<=c+2; k++)
                         if(grid[r][k] === 1) sg2_coin++;
                     let sum = sg1_coin+sg2_coin;
                     maxCoin = Math.max(maxCoin, sum);
