bc8812c59481b8309757966721854aa1f060695d
Gomin <bighead0831@naver.com>
2025-07-11 21:07:57 +0900

    Title: 아름다운 수열 2, Time: 170ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250711/아름다운 수열 2/beautiful-sequence-2.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+
+const [N, M] = input[0].split(" ").map(Number);
+const A = input[1].split(" ").map(Number);
+const B = input[2].split(" ").map(Number);
+
+let result = 0;
+for(let i=0; i<=N-M; i++) {
+    let tmpA = []; // 수열 A 중 크기 M 구간
+    for(let j=0; j<M; j++) 
+        tmpA.push(A[i+j]);
+        
+    let chkB = Array(M).fill(1);
+    for(let j=0; j<M; j++)
+        for(let k=0; k<M; k++)
+            if(tmpA[j] === B[k]) 
+                chkB[k]--;
+
+    let cnt = 0;
+    for(let j=0; j<M; j++)
+        if(chkB[j] === 1) cnt++;
+    if(cnt === 0) result++;
+}
+
+console.log(result);
