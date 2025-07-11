192c6c2f4061ff651d36d8da6ffcf97b80a07a99
Gomin <bighead0831@naver.com>
2025-07-11 21:21:41 +0900

    Title: 아름다운 수열 2, Time: 171ms, Memory: 15MB, Status: Passed - Codetree

--- 250711/아름다운 수열 2/beautiful-sequence-2.js
+++ 250711/아름다운 수열 2/beautiful-sequence-2.js
@@ -5,22 +5,23 @@ const [N, M] = input[0].split(" ").map(Number);
 const A = input[1].split(" ").map(Number);
 const B = input[2].split(" ").map(Number);
 
+function cmp(a,b) {return a-b;}
+
 let result = 0;
 for(let i=0; i<=N-M; i++) {
     let tmpA = []; // 수열 A 중 크기 M 구간
     for(let j=0; j<M; j++) 
         tmpA.push(A[i+j]);
 
-    let chkA = Array(M).fill(1);
-    for(let j=0; j<M; j++)
-        for(let k=0; k<M; k++)
-            if(tmpA[k] === B[j]) 
-                chkA[k]--;
-
+    let tmpB = B;
+    tmpA.sort(cmp); // 오름차순 정렬
+    tmpB.sort(cmp); // 오름차순 정렬
+    
     let cnt = 0;
     for(let j=0; j<M; j++)
-        if(chkA[j] === 1) cnt++;
-    if(cnt === 0) result++;
+        if(tmpA[j] === tmpB[j]) cnt++;
+
+    if(cnt === M) result++;
 }
 
 console.log(result);
