ef0261755f9dd17170a56ee04878183ce5b57fc0
Gomin <bighead0831@naver.com>
2025-07-11 21:13:17 +0900

    Title: 아름다운 수열 2, Time: 181ms, Memory: 15MB, Status: Wrong Answer - Codetree

--- 250711/아름다운 수열 2/beautiful-sequence-2.js
+++ 250711/아름다운 수열 2/beautiful-sequence-2.js
@@ -10,16 +10,16 @@ for(let i=0; i<=N-M; i++) {
     let tmpA = []; // 수열 A 중 크기 M 구간
     for(let j=0; j<M; j++) 
         tmpA.push(A[i+j]);
-        
-    let chkB = Array(M).fill(1);
+
+    let chkA = Array(M).fill(1);
     for(let j=0; j<M; j++)
         for(let k=0; k<M; k++)
-            if(tmpA[j] === B[k]) 
-                chkB[k]--;
+            if(tmpA[k] === B[j]) 
+                chkA[k]--;
 
     let cnt = 0;
     for(let j=0; j<M; j++)
-        if(chkB[j] === 1) cnt++;
+        if(chkA[j] === 1) cnt++;
     if(cnt === 0) result++;
 }
 
