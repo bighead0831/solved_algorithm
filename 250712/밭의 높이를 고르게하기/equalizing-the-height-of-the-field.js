c253c1eaf088fb0f7668b75e462330efb511dfb4
Gomin <bighead0831@naver.com>
2025-07-12 23:15:20 +0900

    Title: 밭의 높이를 고르게하기, Time: 178ms, Memory: 15MB, Status: Passed - Codetree

--- 250712/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.js
+++ 250712/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.js
@@ -1,25 +1,18 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
-const [n, h, t] = input[0].split(' ').map(Number);
+const [N, H, T] = input[0].split(' ').map(Number);
 const arr = input[1].split(' ').map(Number);
 
 // Please Write your code here.
-let minEnergy = 200*n;
-for(let i=0; i<n-t; i++) {
+let minEnergy = 200*N;
+for(let i=0; i<=N-T; i++) {
     let energy = 0;
-    for(let j=i; j<i+t; j++) {
+    for(let j=i; j<i+T; j++) {
         let height = arr[j];
-        while(height != h) {
-            if(height < h) {
-                height++;
-                energy++;
-            } else if(height > h) {
-                height--;
-                energy++;
-            } else {
-                break;
-            }
+        while(height != H) {
+            if(height < H) {height++; energy++;}
+            else if(height > H) {height--; energy++;}
         }
     }
     minEnergy = Math.min(minEnergy, energy);
