a7269c0fa2b8a7b40e78d225f636ac5c6471d131
Gomin <bighead0831@naver.com>
2025-07-15 13:49:53 +0900

    Title: 개발자의 능력 3, Time: 178ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250715/개발자의 능력 3/ability-of-developer-3.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const arr = input[0].trim().split(' ').map(Number);
+
+// Please Write your code here.
+let result = 30000000;
+
+let all = 0;
+for(let i=0; i<6; i++) all+=arr[i];
+
+let team1 = 0;
+for(let i=0; i<4; i++) {
+    team1 = 0;
+    for(let j=i+1; j<5; j++) {
+        for(let k=j; k<6; k++) {
+            team1 += arr[i] + arr[j] + arr[k];
+            let team2 = all - team1;
+            result = Math.min(result, Math.abs(team1-team2));
+        }
+    }
+}
+
+console.log(result);
