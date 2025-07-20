793d2e8c75ae7c3f608f0c81aea4acc10095904c
Gomin <bighead0831@naver.com>
2025-07-20 23:52:36 +0900

    Title: 겹치지 않는 선분 2, Time: 173ms, Memory: 15MB, Status: Wrong Answer - Codetree

--- 250720/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.js
+++ 250720/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.js
@@ -8,9 +8,10 @@ let cnt = 0;
 for(let i=0; i<n-1; i++) {
     let touched = 0;
     let [x1, x2] = segments[i];
-    for(let j=i+1; j<n; j++) {
-        let [cx1, cx2] = segments[j];
+    for(let j=0; j<n; j++) {
+        if(i===j) continue; // i와 (i를 제외한 모든 선분) 확인
 
+        let [cx1, cx2] = segments[j];
         // 겹칠 경우 touched 증가
         if(x1<cx1 && x2>cx2) touched++;
         else if(x1>cx1 && x2<cx2) touched++;
