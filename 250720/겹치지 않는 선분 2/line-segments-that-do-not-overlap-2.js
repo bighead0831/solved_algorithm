0d5aa07e0a25b98cd8448b7104078062a726a09f
Gomin <bighead0831@naver.com>
2025-07-20 23:42:59 +0900

    Title: 겹치지 않는 선분 2, Time: 174ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250720/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+const segments = input.slice(1, n + 1).map(line => line.split(' ').map(Number));
+// Please Write your code here.
+
+let cnt = 0;
+for(let i=0; i<n-1; i++) {
+    let touched = 0;
+    let [x1, x2] = segments[i];
+    for(let j=i+1; j<n; j++) {
+        let [cx1, cx2] = segments[j];
+
+        // 겹칠 경우 touched 증가
+        if(x1<cx1 && x2>cx2) touched++;
+        else if(x1>cx1 && x2<cx2) touched++;
+    }
+    // 한번도 안 겹쳤을 경우 cnt 증가
+    if(touched===0) cnt++;
+}
+
+console.log(cnt);
