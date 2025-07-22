790d20cc9ec0dc0cf82d6f8f848b26fcf62824ca
Gomin <bighead0831@naver.com>
2025-07-22 21:03:02 +0900

    Title: 이상한 폭탄 2, Time: 182ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250722/이상한 폭탄 2/strange-bomb-2.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const [n, k] = input[0].split(' ').map(Number);
+const nums = input.slice(1, n + 1).map(Number);
+
+let boom = -1;
+for(let i=0; i<n; i++) {
+    for(let j=0; j<n; j++) {
+        if(i===j) continue;
+        if(nums[i] === nums[j] && Math.abs(i-j)<=k) boom = nums[i]; 
+    }
+}
+
+console.log(boom);
