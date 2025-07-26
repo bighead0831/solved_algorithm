40beebb1a7e0fd02b7f2fe8546cde8448c742e31
Gomin <bighead0831@naver.com>
2025-07-26 18:35:10 +0900

    Title: 야바위, Time: 173ms, Memory: 15MB, Status: Passed - Codetree

--- 250726/야바위/ya-rock.js
+++ 250726/야바위/ya-rock.js
@@ -4,22 +4,21 @@ const n = Number(input[0]);
 const commands = input.slice(1).map(line => line.split(' ').map(Number));
 // Please Write your code here.
 
-let appPos = -1; // Appropriate Position
+// let appPos = -1; // Appropriate Position
 let arr = new Array(3).fill(0);
 let maxCnt=0;
 for(let i=0; i<3; i++) {
     arr[i] = 1;
     let cnt=0;
     for(let j=0; j<n; j++) {
-        let tmp = arr[commands[j][0]];
-        arr[commands[j][0]] = arr[commands[j][1]];
-        arr[commands[j][1]] = tmp;
-       if(arr[commands[j][2]]===1) cnt++;
+        let tmp = arr[commands[j][0]-1];
+        arr[commands[j][0]-1] = arr[commands[j][1]-1];
+        arr[commands[j][1]-1] = tmp;
+       if(arr[commands[j][2]-1]===1) cnt++;
     }
     maxCnt = Math.max(maxCnt, cnt);
-    if(maxCnt === cnt) appPos = i;
-
-    arr[i] = 0;
+    // if(maxCnt === cnt) appPos = i+1;
+    arr = Array(3).fill(0); // initiate Pure State
 }
 
-console.log(appPos);
+console.log(maxCnt);
