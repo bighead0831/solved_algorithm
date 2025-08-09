9a7c98d14d7d9c9c65eb73dce7272c70e070c8cb
Gomin <bighead0831@naver.com>
2025-08-09 23:10:03 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 242ms, Memory: 16MB, Status: Passed - Codetree

--- 250809/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.js
+++ 250809/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.js
@@ -10,7 +10,7 @@ for(let i=0; i<n; i++) { // (0~n-1)에서 시작하는 부분문자열
     const chkStr = []; // 검사할 부분문자열
     for(let j=0; j<n-i; j++) { // 부분문자열의 길이
         chkStr.push(string[i+j]);
-        for(let k=i+j+1; k<=n-chkStr.length; k++) { // 부분문자열 설정 이후 문자열에서 탐색
+        for(let k=i+1; k<=n-chkStr.length; k++) { // 부분문자열 설정 이후 문자열에서 탐색
             if(chkStr[0] === string[k]) { // 부분문자열의 첫 글자 발견하면 검사 진행
                 let isOk = true;
                 for(let l=0; l<chkStr.length; l++)
