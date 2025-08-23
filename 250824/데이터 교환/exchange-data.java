778391aaf43d7c82956d0f14b3a463e82c4dff0a
Gomin <bighead0831@naver.com>
2025-08-24 00:19:54 +0900

    Title: 데이터 교환, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250824/데이터 교환/exchange-data.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int a=5, b=6, c=7;
+        int tmpa=a;
+        a=b;
+        b=c;
+        c=tmpa
+        System.out.println(a);
+        System.out.println(a);
+    }
+}
