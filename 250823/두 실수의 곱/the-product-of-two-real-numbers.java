30a22744e9043af738cc2dfb42b64bce71e0773a
Gomin <bighead0831@naver.com>
2025-08-23 18:19:32 +0900

    Title: 두 실수의 곱, Time: 95ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250823/두 실수의 곱/the-product-of-two-real-numbers.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        double a = 5.26, b = 8.27;
+        double result = a*b;
+        System.out.printf("%.3f", result);
+    }
+}
