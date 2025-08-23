503ce398d31a75cfbd70fc8e965fd9824d1de158
Gomin <bighead0831@naver.com>
2025-08-24 00:21:23 +0900

    Title: 데이터 교환, Time: 86ms, Memory: 9MB, Status: Passed - Codetree

--- 250824/데이터 교환/exchange-data.java
+++ 250824/데이터 교환/exchange-data.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         int a=5, b=6, c=7;
-        int tmpa=a;
-        a=b;
-        b=c;
-        c=tmpa;
+        int tmpb=b, tmpc=c;
+        b=a;
+        c=tmpb;
+        a=tmpc;
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
