60e112a5866573ba6c0d167cef1ed97158cdbd6a
Gomin <bighead0831@naver.com>
2025-11-24 21:37:35 +0900

    Title: 삼항연산자, Time: 128ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251124/삼항연산자/ternary-operator.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int score = sc.nextInt();
+
+        int result = score==100?0:1;
+        if(result==0) System.out.println("pass");
+        else System.out.println("failure");
+    }
+}
