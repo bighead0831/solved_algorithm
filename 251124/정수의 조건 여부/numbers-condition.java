ccfa00d81de21a6135b00048d618e193eb41a2ea
Gomin <bighead0831@naver.com>
2025-11-24 21:30:23 +0900

    Title: 정수의 조건 여부, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251124/정수의 조건 여부/numbers-condition.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int result = 0;
+        if(a>=113)
+            result = 1;
+        System.out.println(result);
+    }
+}
