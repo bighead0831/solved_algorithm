615bc3e257e4825c58da31ae486b50e567456072
Gomin <bighead0831@naver.com>
2025-11-25 20:44:34 +0900

    Title: 알파벳에 따른 평가, Time: 117ms, Memory: 10MB, Status: Passed - Codetree

--- 251125/알파벳에 따른 평가/evaluation-by-alphabet.java
+++ 251125/알파벳에 따른 평가/evaluation-by-alphabet.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        char c = sc.newChar();
+        char c = sc.next().charAt(0);
         if(c=='S')
             System.out.println("Superior");
         else if(c=='A')
