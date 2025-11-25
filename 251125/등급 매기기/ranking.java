5c3e3fc0cdec0eb2a6f41d2abf21f459859654f2
Gomin <bighead0831@naver.com>
2025-11-25 20:36:04 +0900

    Title: 등급 매기기, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251125/등급 매기기/ranking.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int score = sc.nextInt();
+        char rank = 'F';
+        if(score>=90)
+            rank = 'A';
+        else if(score>=80)
+            rank = 'B';
+        else if(score>=70)
+            rank = 'C';
+        else if(score>=60)
+            rank = 'D';
+        else   
+            rank = 'F';
+        System.out.println(rank);
+    }
+}
