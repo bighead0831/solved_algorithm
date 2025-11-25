09dc393167b0583eecd76535ce1b95d17e846e78
Gomin <bighead0831@naver.com>
2025-11-26 00:52:15 +0900

    Title: 시력 검사, Time: 132ms, Memory: 11MB, Status: Passed - Codetree

---
+++ 251126/시력 검사/eye-test.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        
+        String s = "";
+        if(a>=1.0&&b>=1.0) s="High";
+        else if(a>=0.5&&b>=0.5) s="Middle";
+        else s="Low";
+
+        System.out.println(s);
+    }
+}
