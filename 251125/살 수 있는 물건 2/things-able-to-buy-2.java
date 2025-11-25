6b10cc1f4e866542e3a192ed175f8e6bc3cb0635
Gomin <bighead0831@naver.com>
2025-11-25 20:44:47 +0900

    Title: 살 수 있는 물건 2, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

--- 251125/살 수 있는 물건 2/things-able-to-buy-2.java
+++ 251125/살 수 있는 물건 2/things-able-to-buy-2.java
@@ -4,11 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int money = sc.nextInt();
-        if(n>=3000)
+        if(money>=3000)
             System.out.println("book");
-        else if(n>=1000)
+        else if(money>=1000)
             System.out.println("mask");
-        else if(n>=500)
+        else if(money>=500)
             System.out.println("pen");
         else   
             System.out.println("no");
