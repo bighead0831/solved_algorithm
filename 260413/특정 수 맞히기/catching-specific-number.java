4132a19a52276491fe83a771424a605c0e0d3707
Gomin <bighead0831@naver.com>
2026-04-13 15:09:14 +0900

    Title: 특정 수 맞히기, Time: 0ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 260413/특정 수 맞히기/catching-specific-number.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String input = br.readLine();
+        while(!input.isEmpty()) {
+            int n = Integer.parseInt(input);
+            if(n>25) sb.append("Lower\n");
+            else if(n<25) sb.append("Higher\n");
+            else sb.append("Good\n");
+            input = br.readLine();
+        }
+        System.out.println(sb.toString());
+    }
+}
