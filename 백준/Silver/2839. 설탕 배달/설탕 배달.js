1b8a7cd988daa950055259c2bd8d998863dc32bb
Gomin <bighead0831@naver.com>
2026-04-16 20:37:45 +0900

    [Silver IV] Title: 설탕 배달, Time: 100 ms, Memory: 9608 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.js
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.js
@@ -1,44 +1,19 @@
 const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
 
-let m = Number(fs.readFileSync(0).toString().trim()); // 배달할 설탕 무게 입력
+const n = parseInt(input[0], 10);
 
-let result = []; // 최소 설탕봉지 갯수 초기화
-let rcnt = 0;
+let bag3 = 0;
+let bag5 = 0;
 
-let realResult = 0;
+bag5 = parseInt(n/5);
+bag3 = parseInt((n-bag5*5)/3);
 
-for (let i=0; i<=parseInt(m/5); i++) {
-    let cnt = i; // 5키로 설탕봉지 갯수를 일단 카운트에 포함
-    
-    if (m == i*5) { // 5키로 봉지만으로도 조건이 충족되었을 때도 result에 저장
-        result[rcnt] = cnt;
-        rcnt++;
-        break;
-    } else {
-        for (let j=0; j<=parseInt((m-i*5)/3); j++) {
-            if (m == 5*i+3*j) { // 입력한 설탕무게와 정확히 일치하면
-                cnt += j; // 기존 포함한 5키로 설탕봉지 갯수에 3키로 설탕봉지 갯수 덧셈
-            }
-        }
-        if (cnt > i) { // 5키로 봉지와 3키로 봉지 갯수의 합 result에 저장
-            result[rcnt] = cnt;
-            rcnt++;
-        }
-    }
+
+while(n!=bag5*5+bag3*3) {
+    if(bag5 != 0) {bag5 -= 1;}
+    bag3 = parseInt((n-bag5*5)/3);
+    if(bag5 === 0 && bag3*3 != n) {bag3 = -1; break;} 
 }
 
-if(rcnt == 0) { // 설탕 무게 입력값 만큼 봉지로 옮길 수 없으면 '-1' 출력
-    realResult = -1;
-} else if(rcnt == 1) { // 봉지갯수 케이스가 한 개일 경우 바로 출력
-    realResult = Number(result[0]);
-} else { // 봉지 갯수 케이스가 여러 개일 경우 비교하여 작은 것을 출력
-    let tmp = result[rcnt-1];
-    for(let k=0; k<rcnt; k++) {
-        if(Number(result[k]) <= tmp) {
-            realResult = Number(result[k])
-            tmp = realResult;
-        }
-    }
-}
-
-console.log(realResult);
+console.log(bag5+bag3);
