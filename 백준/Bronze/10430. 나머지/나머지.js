7a76e0175eb3b2647df6763fd4642c6a55774c4b
Gomin <bighead0831@naver.com>
2026-04-16 20:36:30 +0900

    [Bronze V] Title: 나머지, Time: 92 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const [a, b, c] = fs.readFileSync(0).toString().trim().split(' ').map(Number);
+
+console.log((a+b)%c);
+console.log(((a%c)+(b%c))%c);
+console.log((a*b)%c);
+console.log(((a%c)*(b%c))%c);
