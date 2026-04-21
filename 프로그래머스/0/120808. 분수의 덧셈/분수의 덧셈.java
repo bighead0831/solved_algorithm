cc47e969007ebc47b843a1be7da1fcb9279a2121
Gomin <bighead0831@naver.com>
2026-04-21 11:03:03 +0900

    [level 0] Title: 분수의 덧셈, Time: 6.60 ms, Memory: 83.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int numer1, int denom1, int numer2, int denom2) {
+        int[] answer = new int[2];
+        answer[0] = numer1*denom2+denom1*numer2;
+        answer[1] = denom1*denom2;
+        int min = answer[0]>answer[1]?answer[1]:answer[0];
+        int div = 0;
+        for(int i=1; i<=min; i++) {
+            if(answer[0]%i==0&&answer[1]%i==0)
+                div = i;
+        }
+        answer[0] /= div;
+        answer[1] /= div;
+        return answer;
+    }
+}
